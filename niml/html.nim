e8d9afafac2bb9bccc1e573b3de17a0de7815b05
Jakub Dóka <mlokogegelmlok@gmail.com>
2021-04-27 16:53:34 +0200

    adding readme, making templating nicer

--- niml/html.nim
+++ niml/html.nim
@@ -18,11 +18,12 @@ template merge(str: string, exp: NimNode): untyped =
 
 proc expand(n: NimNode, depth: int = 0): NimNode {.compileTime.} =
     let space = "  ".repeat(max(depth, 0))
-    if n.matches(Call[==ident("exp"), @body]):
+    if n.matches(Prefix[==ident("@"), @body]):
         if depth == -1 or not defined(debug):
-            return body
+            return quote do:
+                $(`body`)
         return quote do:
-            `space` & `body` & "\n"
+            `space` & $(`body`) & "\n"
     
     if n.matches(Ident()):
         when defined(debug):
@@ -42,6 +43,9 @@ proc expand(n: NimNode, depth: int = 0): NimNode {.compileTime.} =
         else:
             error("invalid syntax")
 
+    if name == ident("divider"):
+        name = ident("div")
+
     result = newLit("")
 
     var buff = ""
@@ -54,11 +58,11 @@ proc expand(n: NimNode, depth: int = 0): NimNode {.compileTime.} =
         buff.add(">" & value.strVal & "</" & name.strVal & ">")
         debug "\n"
         return newLit(buff)
-    elif n.matches(Command[_, Call[==ident("exp"), @body]]):
+    elif n.matches(Command[_, Prefix[==ident("@"), @body]]):
         buff.add(">")
         let lit = newLit(buff)
         result = quote do:
-            `result` & `lit` & `body`
+            `result` & `lit` & $(`body`)
         buff = "</" & name.strVal & ">"
         debug "\n"
         let lit2 = newLit(buff)
@@ -67,22 +71,25 @@ proc expand(n: NimNode, depth: int = 0): NimNode {.compileTime.} =
         return
     elif n.matches(Command[_, until @args is StmtList(), .._]):
         for arg in args:
-            let argOK = arg.matches:
-                        Infix[==ident("&"), @key, @value]
-            if not argOK:
-                error(
-                    "element attribute has to be in form ˙key & \"value\"˙, key can optionally " & 
-                    "be inside ``, value can be an expression that evaluates to string"
-                )
-            buff.add(" " & key.toString & "=")
-            if value.matches(StrLit()):
-                buff.add(value.repr)
+            if arg.matches(Ident()):
+                buff.add(" " & arg.toString)
             else:
-                let lit = newLit(buff)
-                let value = expand(value, -1)
-                result = quote do:
-                    `result` & `lit` & "\"" & `value` & "\""
-                buff = ""
+                let argOK = arg.matches:
+                            Infix[==ident("&"), @key, @value]
+                if not argOK:
+                    error(
+                        "element attribute has to be in form ˙key & \"value\"˙, key can optionally " & 
+                        "be inside ``, value can be an expression that evaluates to string"
+                    )
+                buff.add(" " & key.toString & "=")
+                if value.matches(StrLit()):
+                    buff.add(value.repr)
+                else:
+                    let lit = newLit(buff)
+                    let value = expand(value, -1)
+                    result = quote do:
+                        `result` & `lit` & "\"" & `value` & "\""
+                    buff = ""
 
     var last = n[n.len-1]
     if last.matches(StmtList()):
@@ -106,16 +113,16 @@ proc expand(n: NimNode, depth: int = 0): NimNode {.compileTime.} =
     result = quote do:
         `result` & `lit`
         
-macro page*(code: untyped): untyped =
-    ## page takes input in form of syntax tree and turns it into string expression
+macro niml*(code: untyped): untyped =
+    ## niml takes input in form of syntax tree and turns it into string expression
     ## witch can also contain dynamic parts, for example:
     ##
     ## .. code-block:: nim
     ##   let
     ##     a = "hello"
     ##     b = 10
-    ##     pg = page: 
-    ##       html attribute&"value", otherAttribute&exp($b), `ugly-attribute`&"brah":
+    ##     pg = niml: 
+    ##       html attribute&"value", otherAttribute&@b, `ugly-attribute`&"brah":
     ##         head:
     ##           title exp(a)
     ##         body:
@@ -126,10 +133,13 @@ macro page*(code: untyped): untyped =
     ##             "this"
     ##
     ## When defining attributes you have to use & instead of = for technical reasons.
-    ## If you need to specify ugly identifier write it as operator. If you enclose 
-    ## something into exp() you can use variables from scope inside, though expression
-    ## has to evaluate into string. You can use --define:debug to view a formatted html
+    ## If you need to specify ugly identifier write it as operator. If you prefix
+    ## something with @ to refer to it, though expression has to evaluate into string 
+    ## of have $ defined. You can use --define:debug to view a formatted html.
     result = newLit("")
+
+    when isMainModule:
+        echo code.treeRepr
 
     for n in code:
         let exp = expand(n)
@@ -139,7 +149,7 @@ macro page*(code: untyped): untyped =
 when isMainModule:
     let brah = "hello there"
     let val = 10
-    let pg = page:
+    var pg = niml:
         html lang & "en", kub & "flee":
             head:
                 meta charset & "UTF-8"
@@ -148,12 +158,12 @@ when isMainModule:
                 title "Fonting"
             body:
                 h1 "Hello there"
-                p hello & exp(brah)
-                p exp(brah)
+                p hello & @brah
+                p @brah
                 p:
-                    exp(brah)
+                    @brah
                     br
-                    exp($(3 + 5 + val))
+                    @(3 + 5 + val)
     assert pg == """<html lang="en" kub="flee">
   <head>
     <meta charset="UTF-8"/>
@@ -173,4 +183,44 @@ when isMainModule:
   </body>
 </html>
 """
+    let ten = 10
+    let text = "text"
 
+    pg = niml:
+        html:
+            head:
+                meta charset & "UTF-8"
+                meta `http-equiv` & "X-UA-Compatible", content & "IE=edge"
+                meta name & "viewport", content&"width=device-width, initial-scale=1.0"
+                title "Niml"
+            body:
+                h1 "Headline"
+                p:
+                    "some text"
+                    br
+                    "some more text"
+                table:
+                    tr:
+                        td @text
+                        td @ten
+                    tr:
+                        td "something else"
+                        td "30"
+                divider attribute & @text, another_attribute & @(ten + 30 - 16), hidden:
+                    "this is actually div but as that is already taken keyword we are using alternative"
+    echo pg
+
+    proc popup(id, callback, message: string, hidden = false): string = 
+        niml:
+            divider id & @id, style & "too lazy for this", hidden & @hidden:
+                h1 @message
+                button id & @(id & "-yes"), onclick & @callback:
+                    "Yes"
+                button onclick & "close-popup", popup_id & @id:
+                    "No"
+
+    pg = niml:
+        @(popup("exit-popup", "exit", "Do you want to exit?", true))
+        @(popup("open-exit-popup", "open-exit", "Do you want to open exit popup?"))
+    
+    echo pg
