9501bedf3acfc9fbe8ba31128f843b8aee0fa5f4
Jakub Dóka <mlokogegelmlok@gmail.com>
2021-04-27 16:59:12 +0200

    restructuring due to wrong dirrectory

---
+++ niml.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Jakub Dóka"
+description   = "html dsl"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 1.4.4", "fusion"
--- src/niml.nim
+++ src/niml.nim
@@ -4,17 +4,17 @@ export html
 proc defaultPage*(title, body: string, styles = ""): string =
   ## defaultPage creates a standard html page with custom title,
   ## styles and body
-  let p = page:
+  let p = niml:
     html lang & "en":
       head:
         meta charset & "UTF-8"
         meta `http-equiv` & "X-UA-Compatible", content & "IE=edge"
         meta name & "viewport", content&"width=device-width, initial-scale=1.0"
-        title exp(title)
+        title @title
         style: 
-          exp(styles)
+          @styles
       body:
-        exp(body)
+        @body
 
   when defined(debug):
     return "<!DOCTYPE html>\n" & p
@@ -22,12 +22,14 @@ proc defaultPage*(title, body: string, styles = ""): string =
     return "<!DOCTYPE html>" & p
 
 when isMainModule:
-  let body = page:
+  let body = niml:
     h1 "Good morning"
     p:
       "How are you today?"
       br
       "Its so interesting any yet so complicated."
   
-  echo defaultPage("something", body)
+  echo defaultHtml("something", body)
+
+  
   
