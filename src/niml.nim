45f7656e3e608c92ad4657627e684f8a9e1f10c0
Jakub Dóka <mlokogegelmlok@gmail.com>
2021-04-28 15:05:11 +0200

    little template tweeking

--- src/niml.nim
+++ src/niml.nim
@@ -6,14 +6,14 @@ proc defaultPage*(title, body: string, styles = ""): string =
   ## styles and body
   let p = niml:
     html lang & "en":
-      head:
+      head id & "head":
         meta charset & "UTF-8"
         meta `http-equiv` & "X-UA-Compatible", content & "IE=edge"
         meta name & "viewport", content&"width=device-width, initial-scale=1.0"
         title @title
         style: 
           @styles
-      body:
+      body id & "body":
         @body
 
   when defined(debug):
